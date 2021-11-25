c6489db77bec9eff420cc54c6c317df3b100c768
MaximeLorin <maxime.lorin56@gmail.com>
2021-11-25 11:41:32 +0100

    home refactorised

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,9 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <h1>Votre Position</h1>
-    <h2>Lattitude: {{ currPos.lat.toFixed(2) }}</h2>
-    <h2>Longitude: {{ currPos.lng.toFixed(2) }}</h2>
+    <h2>Lattitude: {{ lat.toFixed(2) }}</h2>
+    <h2>Longitude: {{ lng.toFixed(2) }}</h2>
+    <input ref="autocomplete" type="text" placeholder="Entrez un lieu" />
     <div ref="mapDiv" style="width: 100%; height: 80vh"></div>
   </div>
 </template>
@@ -15,10 +16,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 require("dotenv").config();
 
-import { computed, ref, onMounted } from "vue";
-import { useGeolocation } from "../useGeolocation";
 import { Loader } from "@googlemaps/js-api-loader";
-
 const GOOGLE_MAPS_API_KEY = process.env.VUE_APP_API_KEY;
 
 export default {
@@ -26,25 +24,37 @@ export default {
   components: {
     HelloWorld,
   },
-  setup() {
-    const { coords } = useGeolocation();
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude,
-    }));
-
-    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
-    const mapDiv = ref(null);
-    console.log(mapDiv);
-    onMounted(async () => {
+  data() {
+    return {
+      lat: 0,
+      lng: 0,
+      type: "",
+      radius: "",
+      places: [],
+    };
+  },
+  async mounted() {
+    try {
+      const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
       await loader.load();
-      new google.maps.Map(mapDiv.value, {
-        center: currPos.value,
-        zoom: 7,
+      this.drawMap();
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  computed: {
+    coordinates() {
+      return `${this.lat},${this.lng}`;
+    },
+  },
+  methods: {
+    drawMap() {
+      new google.maps.Map(this.$refs.mapDiv, {
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13,
+        mapTypeId: "roadmap",
       });
-    });
-
-    return { currPos, mapDiv };
+    },
   },
 };
 </script>
